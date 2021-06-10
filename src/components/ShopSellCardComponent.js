6ef86ef200f6da86e7fa14895cefa7ef39fb4729
Sukhmahey <sukhmahey4@gmail.com>
2021-06-10 17:23:06 +0530

    "Final Bug fixed 2"

--- src/components/ShopSellCardComponent.js
+++ src/components/ShopSellCardComponent.js
@@ -76,7 +76,7 @@ const ShopSellCardComponent = ({
                 phone,
                 address,
                 photo,
-                id,
+                shopId,
                 desc,
               });
             }}
